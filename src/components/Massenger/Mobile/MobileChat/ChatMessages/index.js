664a53be0f7f05377df7f71d5a20c7fc259b1f91
ahura-del <ahuradel@gmail.com>
2021-11-21 18:56:57 +0330

    create and design chat message component

---
+++ src/components/Massenger/Mobile/MobileChat/ChatMessages/index.js
@@ -0,0 +1,32 @@
+import { Container } from '@mui/material'
+import React from 'react'
+import ChatText from './ChatText'
+function Index() {
+    return (
+        <Container  sx={{height:"100%"}}>
+           <ChatText dir="recive"/>
+        <ChatText dir="send"/>
+        <ChatText dir="send"/>
+        <ChatText dir="recive"/>
+        <ChatText dir="recive"/>
+        <ChatText dir="send"/>
+        <ChatText dir="send"/>
+        <ChatText dir="recive"/>
+        <ChatText dir="recive"/>
+        <ChatText dir="send"/>
+        <ChatText dir="send"/>
+        <ChatText dir="recive"/>
+        <ChatText dir="recive"/>
+        <ChatText dir="send"/>
+        <ChatText dir="send"/>
+        <ChatText dir="recive"/>
+        <ChatText dir="recive"/>
+        <ChatText dir="send"/>
+        <ChatText dir="send"/>
+        <ChatText dir="recive"/>
+
+        </Container>
+    )
+}
+
+export default Index
--- src/components/Massenger/Mobile/MobileChat/index.js
+++ src/components/Massenger/Mobile/MobileChat/index.js
@@ -1,16 +1,17 @@
 import { Grid } from '@mui/material'
 import React from 'react'
 import ChatHeader from './ChatHeader'
+import ChatMessage from './ChatMessages'
 function Index() {
     return (
         <Grid container direction="column" style={{height:"100vh" , overflow:"hidden" , width:"100%"}}>
             <Grid item xs={1} sx={{bgcolor:"#2F3135" , width:"100%" , height:"100%"}}>
                 <ChatHeader /> 
             </Grid>
-            <Grid item xs={10}>
-                <p>chat message</p>
+            <Grid item xs={10} sx={{pt:1 , bgcolor:"#363A3F" , overflowY:"auto" , height:"100%"  , width:"100%"}}>
+               <ChatMessage />
             </Grid>
-            <Grid item xs={1}>
+            <Grid item xs={1} sx={{bgcolor:"blue"}}>
                 <p>chat footer</p>
             </Grid>
         </Grid>
